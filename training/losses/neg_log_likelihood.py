8e02ee5aa9434618566715c9bcd6857324f5c899
HaozeChang <haozechaung@gmail.com>
2024-11-25 18:39:16 +0800

    add PDC ATTEn

--- training/losses/neg_log_likelihood.py
+++ training/losses/neg_log_likelihood.py
@@ -4,6 +4,93 @@ import math
 import torch.nn as nn
 
 
+class NLLMixtureLaplaceandGaussian:
+    """ Computes Negative Log Likelihood loss for a Mixture of Laplace distributions and Gaussian distributions. """
+    def __init__(self, reduction='weighted_sum_normalized', ratio=1.0):
+        """
+        Args:
+            reduction: str, type of reduction to apply to loss
+            ratio:
+        """
+        super().__init__()
+        self.reduction = reduction
+        self.ratio = ratio
+
+    #def __call__(self, gt_flow, est_flow, log_var_alpha,log_var_beta, weight_map_alpha,weight_map_beta, mask=None):
+    def __call__(self, gt_flow, est_flow, log_var ,weight_map, mask=None):
+        """
+        Args:
+            gt_flow: ground-truth flow field, shape (b, 2, H, W)
+            est_flow: estimated flow field, shape (b, 2, H, W)
+            log_var_alpha: estimated log variances for alpha, shape (b, nbr_components, H, W)
+            log_var_beta: estimated log variances for beta, shape (b, nbr_components, H, W)
+            weight_map_alpha: estimated weights map (alpha) for the mixture model, each corresponding to a log variance,
+                        shape (b, nbr_components, H, W)
+            weight_map_beta: estimated weights map (beta) for the mixture model, each corresponding to a log variance,
+            shape (b, nbr_components, H, W)
+            mask: valid mask, where the loss is computed. shape (b, 1, H, W)
+        """
+        # divide into two parts
+        b,c,h,w = weight_map.shape()
+        b,c1,h1,w1 = log_var.shape()
+        assert (c==c1 and h==h1 and w==w1), "must have same shape" 
+        weight_map_alpha = weight_map[:,:c//2,:,:] # *! this part alpha is for Gaussian beta is for Lapalacian
+        weight_map_beta = weight_map[:,c//2:,:,:]
+        log_var_alpha = log_var[:,:c//2,:,:]
+        log_var_beta = log_var[:,c//2:,:,:]
+
+        b, _, h, w = gt_flow.shape
+        #l1 = torch.logsumexp(weight_map, 1, keepdim=True)
+        weights_alpha_softmax = 0.5*nn.Softmax(weight_map_alpha)
+        exp_part = -1*math.sqrt(2)*torch.sum(torch.abs((gt_flow - est_flow)*self.ratio), 1, keepdim=True)*torch.exp(-0.5*log_var_alpha)
+        tmp1 = math.log(2)+log_var_alpha-math.log(torch.exp(exp_part))
+        l1 = torch.sum(weights_alpha_softmax*tmp1)
+        # shape will be b,1,h,w
+        
+        weights_beta_softmax = 0.5*nn.Softmax(weight_map_beta)
+        tmp2 = math.log(2*math.pi)+log_var_beta
+        exp_part = -1*torch.sum(torch.dist((gt_flow - est_flow)*self.ratio,p=2)**2)*torch.exp(-1*math.log(2)-log_var_alpha)
+        #exp_part = -1*torch.exp(-1*math.log(2))*torch.exp(-1*log_var_beta)*torch.sum(torch.dist((gt_flow - est_flow)*self.ratio,p=2)**2)
+        l2 = tmp2-math.log(torch.exp(exp_part))
+        l2 = torch.sum(weights_beta_softmax*l2)
+        #reg = math.sqrt(2)*torch.sum(torch.abs((gt_flow - est_flow)*self.ratio), 1, keepdim=True)  # shape will be b,1,h,w
+        #exponent = weight_map - math.log(2) - log_var - reg * torch.exp(-0.5*log_var)
+        #l2 = torch.logsumexp(exponent, 1, keepdim=True)
+
+        loss = l1 + l2
+
+        if mask is not None:
+            mask = ~torch.isnan(loss.detach()) & ~torch.isinf(loss.detach()) & mask
+        else:
+            mask = ~torch.isnan(loss.detach()) & ~torch.isinf(loss.detach())
+
+        if torch.isnan(loss.detach()).sum().ge(1) or torch.isinf(loss.detach()).sum().ge(1):
+            # debugging
+            print('mask or inf in the loss ! ')
+
+        if self.reduction == 'mean':
+            if mask is not None:
+                loss = torch.masked_select(loss, mask).mean()
+            else:
+                loss = loss.mean()
+            return loss
+        elif 'weighted_sum' in self.reduction:
+            if mask is not None:
+                loss = loss * mask.float()
+                L = 0
+                for bb in range(0, b):
+                    norm_const = float(h)*float(w) / (mask[bb, ...].sum().float() + 1e-6)
+                    L += loss[bb][mask[bb, ...] != 0].sum() * norm_const
+                if 'normalized' in self.reduction:
+                    return L / b
+                return L
+
+            if 'normalized' in self.reduction:
+                return loss.sum() / b
+            return loss.sum()
+        else:
+            raise ValueError
+        
 class NLLMixtureLaplace:
     """ Computes Negative Log Likelihood loss for a Mixture of Laplace distributions. """
     def __init__(self, reduction='weighted_sum_normalized', ratio=1.0):
@@ -67,8 +154,7 @@ class NLLMixtureLaplace:
             return loss.sum()
         else:
             raise ValueError
-
-
+        
 class NLLMixtureGaussian:
     """ Computes Negative Log Likelihood loss for a Mixture of Gaussian distributions. """
     def __init__(self, reduction='weighted_sum_normalized', ratio=1.0):
